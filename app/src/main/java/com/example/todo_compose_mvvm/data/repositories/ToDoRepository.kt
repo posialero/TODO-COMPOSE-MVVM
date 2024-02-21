2e177fd81b6941a8eafc952e60205cdacdd4a4c6
Daniel Najdrowski <danielnajdrowski@gmail.com>
2024-02-21 15:12:57 +0100

    Create SharedViewModel.kt

--- app/src/main/java/com/example/todo_compose_mvvm/data/repositories/ToDoRepository.kt
+++ app/src/main/java/com/example/todo_compose_mvvm/data/repositories/ToDoRepository.kt
@@ -2,9 +2,11 @@ package com.example.todo_compose_mvvm.data.repositories
 
 import com.example.todo_compose_mvvm.data.ToDoDao
 import com.example.todo_compose_mvvm.data.models.ToDoTask
+import dagger.hilt.android.scopes.ViewModelScoped
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
+@ViewModelScoped
 class ToDoRepository @Inject constructor(private val toDoDao: ToDoDao) {
 
     val getAllTasks: Flow<List<ToDoTask>> = toDoDao.getAllTasks()
