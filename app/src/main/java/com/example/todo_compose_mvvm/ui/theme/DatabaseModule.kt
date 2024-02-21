f6034a93b2ad0c43d20b3ed46a558398aedcad0e
Daniel Najdrowski <danielnajdrowski@gmail.com>
2024-02-21 15:02:33 +0100

    Setup Dagger Hilt dependencies.

---
+++ app/src/main/java/com/example/todo_compose_mvvm/ui/theme/DatabaseModule.kt
@@ -0,0 +1,31 @@
+package com.example.todo_compose_mvvm.ui.theme
+
+import android.content.Context
+import androidx.room.Room
+import com.example.todo_compose_mvvm.data.ToDoDatabase
+import com.example.todo_compose_mvvm.util.Constants
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Singleton
+    @Provides
+    fun provideDatabase(
+        @ApplicationContext context: Context
+    ) = Room.databaseBuilder(
+        context,
+        ToDoDatabase::class.java,
+        Constants.DATABASE_NAME
+    ).build()
+
+    @Singleton
+    @Provides
+    fun provideDao(database: ToDoDatabase) = database.toDoDao()
+}
